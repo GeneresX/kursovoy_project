595963a1449e53bd04ba1e3e0a6bf3f4a6926e4e
GeneresX <57197624+GeneresX@users.noreply.github.com>
2020-12-29 01:15:32 +0300

    Update wavefindpath.cpp

--- wavefindpath.cpp
+++ wavefindpath.cpp
@@ -9,54 +9,15 @@
 ScAddr graph, rrel_arcs, rrel_nodes, graph_nodes, graph_arcs, max_path;
 std::unique_ptr<ScMemoryContext> context;
 
-bool set_is_not_empty ( ScAddr& set){
-    ScIterator3Ptr check = context->Iterator3(set,ScType::EdgeAccessConstPosPerm,ScType(0));
-    if (check->Next()) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-void get_edge_vertexes ( ScAddr edge, ScAddr &v1, ScAddr &v2)
+bool exists_in_set(ScAddr set, ScAddr element)
 {
-    v1 = context->GetEdgeSource(edge);
-    v2 = context->GetEdgeTarget(edge);
-}
-
-bool find_vertex_in_set (ScAddr set, ScAddr vertex)
-{
-    ScIterator3Ptr location = context->Iterator3(
-        set,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0)
-    ); // Create iterator to find elements belongs to the set
-
-    while (location->Next()) // Getting every element in set
-    {
-        ScAddr element = location->Get(2);
-
-        if (vertex == element)
-            return true;
-    }
-    return false;
-}
-
-bool find_arc_in_set(const ScAddr& set, ScAddr arc)
-{
-    ScIterator3Ptr search = context->Iterator3(
-        set,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0)
+    ScIterator3Ptr it = context->Iterator3(
+            set,
+            ScType::EdgeAccessConstPosPerm,
+            element
     );
 
-    while (search->Next())
-    {
-        if (context->GetEdgeTarget(arc) == context->GetEdgeTarget(search->Get(2))
-        && context->GetEdgeSource(arc) == context->GetEdgeSource(search->Get(2)))
-            return true;
-    }
-    return false;
+    return it->Next();
 }
 
 void add_to_set(const ScAddr& set, const ScAddr& element)
@@ -70,7 +31,7 @@ void erase_from_set(const ScAddr& set, const ScAddr& element)
 
     if (it->Next())
     {
-        ScAddr edge = it->Get(2);
+        ScAddr edge = it->Get(1);
         context->EraseElement(edge);
     }
 }
@@ -87,106 +48,62 @@ int get_set_size(const ScAddr& set)
     return size;
 }
 
+void get_edge_vertexes ( ScAddr edge, ScAddr &v1, ScAddr &v2)
+{
+    v1 = context->GetEdgeSource(edge);
+    v2 = context->GetEdgeTarget(edge);
+}
+
 void clear_set(const ScAddr& set)
 {
-    ScIterator3Ptr it = context->Iterator3(
-        set,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0)
-    );
-
-    while (it->Next())
+    while (true)
     {
-        ScAddr set_element = it->Get(2);
-        erase_from_set(set, set_element);
+        ScIterator3Ptr it = context->Iterator3(
+                set,
+                ScType::EdgeAccessConstPosPerm,
+                ScType(0)
+        );
+
+        bool removed = false;
+        while (it->Next())
+        {
+            ScAddr set_element = it->Get(2);
+            erase_from_set(set, set_element);
+
+            removed = true;
+        }
+
+        if (!removed)
+            break;
     }
 }
 
-void print_graph ()
+void print_graph()
 {
-    ScAddr v1, v2, printed_vertex;
-
-    printed_vertex = context->CreateNode(ScType::Const);
+    ScAddr v1, v2;
 
     std::cout << "Graph " ;
     printEl(context, graph);
     std::cout<<":" << std::endl;
 
-    ScIterator3Ptr arcs_it = context->Iterator3(graph_arcs,ScType::EdgeAccessConstPosPerm,ScType(0));
-
-    while (arcs_it->Next())
-    {
-        get_edge_vertexes (arcs_it->Get(2), v1, v2);
-        printEl(context, v1);
-        std::cout << " => ";
-        printEl(context, v2);
-        std::cout << std::endl;
-
-        if (!find_vertex_in_set(v1, printed_vertex))
-            context->CreateEdge(ScType::EdgeAccessConstPosPerm,printed_vertex, v1);
-        if (!find_vertex_in_set(v2, printed_vertex))
-            context->CreateEdge(ScType::EdgeAccessConstPosPerm,printed_vertex, v2);
-    }
-
-    ScIterator3Ptr nodes_it = context->Iterator3(graph_nodes,ScType::EdgeAccessConstPosPerm,ScType(0));
-
-    while (nodes_it->Next())
-    {
-        if (find_vertex_in_set(nodes_it->Get(2), printed_vertex))
-        {
-            printEl(context, nodes_it->Get(2));
-            std::cout << std::endl;
-        }
-    }
-    std::cout<<std::endl;
-}
-
-void print_max_cut ()
-{
-    ScAddr v1, v2, printed_vertex;
-
-    printed_vertex = context->CreateNode(ScType::Const);
-
-    std::cout << "Graph max cut:" << std::endl;
-
-    ScIterator3Ptr arcs_it = context->Iterator3(
-        graph_arcs,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0)
-    );
-
-    while (arcs_it->Next())
-    {
-        if(!find_arc_in_set(max_path,arcs_it->Get(2)))
-        {
-            get_edge_vertexes(arcs_it->Get(2), v1, v2);
-            printEl(context, v1);
-            std::cout << " => ";
-            printEl(context, v2);
-            std::cout << std::endl;
-
-            if (!find_vertex_in_set(v1, printed_vertex))
-                context->CreateEdge(ScType::EdgeAccessConstPosPerm,printed_vertex, v1);
-            if (!find_vertex_in_set(v2, printed_vertex))
-                context->CreateEdge(ScType::EdgeAccessConstPosPerm,printed_vertex, v2);
-        }
-    }
-
-    ScIterator3Ptr nodes_it = context->Iterator3(
-            graph_nodes,
+    ScIterator3Ptr it = context->Iterator3(
+            graph_arcs,
             ScType::EdgeAccessConstPosPerm,
             ScType(0)
-        );
+    );
 
-    while (nodes_it->Next())
+    while (it->Next())
     {
-        if (find_vertex_in_set(printed_vertex, nodes_it->Get(2)))
-        {
-            printEl(context, nodes_it->Get(2));
-            std::cout << std::endl;
-        }
+        get_edge_vertexes(it->Get(2), v1, v2);
+
+        printEl(context, v1);
+        std::cout<<" => ";
+        printEl(context, v2);
+
+        std::cout << std::endl;
     }
-    std::cout<<std::endl;
+
+    std::cout << std::endl;
 }
 
 void copySet(const ScAddr& destination, const ScAddr& source)
@@ -194,9 +111,9 @@ void copySet(const ScAddr& destination, const ScAddr& source)
     clear_set(destination);
 
     ScIterator3Ptr it = context->Iterator3(
-        source,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0)
+            source,
+            ScType::EdgeAccessConstPosPerm,
+            ScType(0)
     );
 
     while (it->Next())
@@ -206,55 +123,84 @@ void copySet(const ScAddr& destination, const ScAddr& source)
     }
 }
 
+void print_max_cut()
+{
+    ScAddr v1, v2;
+
+    std::cout<<"Max cut" <<":" << std::endl;
+
+    ScIterator3Ptr it = context->Iterator3(
+            max_path,
+            ScType::EdgeAccessConstPosPerm,
+            ScType(0));
+
+    while (it->Next())
+    {
+        get_edge_vertexes( it->Get(2), v1, v2);
+
+        printEl(context, v1);
+        std::cout<<" => ";
+        printEl(context, v2);
+
+        std::cout << std::endl;
+    }
+}
+
 void depth_search(const ScAddr& edge, const ScAddr& visited_nodes, const ScAddr& path)
 {
-    ScIterator3Ptr it = context->Iterator3(
+    ScIterator3Ptr edge_it = context->Iterator3(
             ScType::Node,
             edge,
-            ScType::Node);
+            ScType::Node
+    );
 
-    ScAddr node;
+    ScAddr next_node;
+    if (edge_it->Next())
+    {
+        next_node = edge_it->Get(2);
+    }
 
-    if(it->Next())
-        node = it->Get(2);
-
-    if (find_vertex_in_set(visited_nodes, node))
+    if (exists_in_set(visited_nodes, next_node))
+    {
         return;
+    }
 
+    add_to_set(visited_nodes, next_node);
     add_to_set(path, edge);
 
-    int path_length = get_set_size(path);
+    int cur_path_length = get_set_size(path);
     int max_path_length = get_set_size(max_path);
 
-    if (path_length > max_path_length)
+    if (cur_path_length > max_path_length)
     {
-        //delete all old data in max_path and copy data from path to it
         copySet(max_path, path);
     }
 
-    ScIterator5Ptr incident = context->Iterator5(
-            node,
+    ScIterator5Ptr it = context->Iterator5(
+            next_node,
             ScType(0),
-            ScType(0),
+            ScType::Node,
             ScType::EdgeAccessConstPosPerm,
-            graph_arcs);
+            graph_arcs
+    );
 
-    while (incident->Next())
+    while (it->Next())
     {
-        ScAddr edge_to_next_node = incident->Get(1);
-        depth_search(edge_to_next_node, visited_nodes, path);
+        ScAddr next_edge = it->Get(1);
+
+        depth_search(next_edge, visited_nodes, path);
     }
 
-    if(edge.IsValid() && find_arc_in_set(path, edge))
-        erase_from_set(path, edge);
+    erase_from_set(visited_nodes, next_node);
+    erase_from_set(path, edge);
 }
 
 void find_max_path()
 {
     ScIterator3Ptr nodes_it = context->Iterator3(
-        graph_nodes,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0)
+            graph_nodes,
+            ScType::EdgeAccessConstPosPerm,
+            ScType(0)
     );
 
     while (nodes_it->Next())
@@ -264,7 +210,7 @@ void find_max_path()
         ScIterator5Ptr incident = context->Iterator5(
                 node,
                 ScType(0),
-                ScType(0),
+                ScType::Node,
                 ScType::EdgeAccessConstPosPerm,
                 graph_arcs
         );
@@ -282,7 +228,6 @@ void find_max_path()
     }
 }
 
-
 void run_test(const std::string& graph_name)
 {
     max_path = context->CreateNode(ScType::Node);
@@ -293,11 +238,11 @@ void run_test(const std::string& graph_name)
     rrel_nodes = context->HelperResolveSystemIdtf("rrel_nodes");
 
     ScIterator5Ptr arcs_it = context->Iterator5(
-        graph,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0),
-        ScType::EdgeAccessConstPosPerm,
-        rrel_arcs
+            graph,
+            ScType::EdgeAccessConstPosPerm,
+            ScType(0),
+            ScType::EdgeAccessConstPosPerm,
+            rrel_arcs
     );
 
     if (arcs_it->Next())
@@ -306,26 +251,24 @@ void run_test(const std::string& graph_name)
     }
 
     ScIterator5Ptr nodes_it = context->Iterator5(
-        graph,
-        ScType::EdgeAccessConstPosPerm,
-        ScType(0),
-        ScType::EdgeAccessConstPosPerm,
-        rrel_nodes
+            graph,
+            ScType::EdgeAccessConstPosPerm,
+            ScType(0),
+            ScType::EdgeAccessConstPosPerm,
+            rrel_nodes
     );
 
-     if (nodes_it->Next())
+    if (nodes_it->Next())
     {
         graph_nodes = nodes_it->Get(2);
     }
 
-     print_graph();
-     find_max_path();
-     if(set_is_not_empty(max_path)){
-         std::cout<<"FULL";
-     }
-     else std::cout<<"EMPTY";
+    find_max_path();
 
-    // print_max_cut();
+    print_graph();
+    print_max_cut();
+
+    std::cout << std::endl;
 }
 
 int main()
